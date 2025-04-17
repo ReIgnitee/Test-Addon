114299dc2b8ba7beca25a7e216c45714fecd4b38
ReIgnitee <cortanav93@gmail.com>
2025-04-17 19:12:31 +0530

    Update Glow-addon.user.js

--- Glow-addon/Glow-addon.user.js
+++ Glow-addon/Glow-addon.user.js
@@ -17,21 +17,26 @@
 
 (function() {
     'use strict';
-
-    //Create a Button Element
-	let Button = document.createElement("Button");
-	Button.innerHTML = "Click Me";
-	Button.style.padding = "10px";
-	Button.style.top = "20px";
-	Button.style.left = "20px";
-	Button.style.position = "fixed";
 	
-	//Appending the Element
-	document.body.appendChild(Button);
-	
-	//Event Listener for the Button
-	Button.addEventListener("click",function(){
-		alert("This is your message dear~");
-	});
-	
+	const waitforlogin = setInterval(() => {
+		if(typeof Player !== "undefined" && Player,Name){
+			clearInterval(waitforlogin);
+			
+			//Create a Button Element
+			const Button = document.createElement("Button");
+			Button.innerHTML = "Click Me";
+			Button.style.padding = "10px";
+			Button.style.top = "20px";
+			Button.style.left = "20px";
+			Button.style.position = "fixed";
+			
+			//Appending the Element
+			document.body.appendChild(Button);
+			
+			//Event Listener for the Button
+			Button.addEventListener("click",function(){
+				alert("This is your message dear~");
+			});
+		}
+	}, 500);
 })();
