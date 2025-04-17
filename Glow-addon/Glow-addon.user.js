746f50464d8db8cd30fd130220574daa62d986b7
ReIgnitee <cortanav93@gmail.com>
2025-04-17 19:37:18 +0530

    Main import part

--- Glow-addon/Glow-addon.user.js
+++ Glow-addon/Glow-addon.user.js
@@ -11,32 +11,7 @@
 // @match https://bondageprojects.com/*
 // @match https://www.bondageprojects.com/*
 // @grant        none
-// @updateURL	https://reignitee.github.io/Test-Addon/Glow-addon/Glow-addon.user.js
-// @downloadURL	https://reignitee.github.io/Test-Addon/Glow-addon/Glow-addon.user.js
+// @run-at       document-idle
 // ==/UserScript==
 
-(function() {
-    'use strict';
-	
-	const waitforlogin = setInterval(() => {
-		if(typeof Player !== "undefined" && Player.Name){
-			clearInterval(waitforlogin);
-			
-			//Create a Button Element
-			const Button = document.createElement("Button");
-			Button.innerHTML = "Click Me";
-			Button.style.padding = "10px";
-			Button.style.top = "20px";
-			Button.style.left = "20px";
-			Button.style.position = "fixed";
-			
-			//Appending the Element
-			document.body.appendChild(Button);
-			
-			//Event Listener for the Button
-			Button.addEventListener("click",function(){
-				alert("This is your message dear~");
-			});
-		}
-	}, 500);
-})();
+import(`https://reignitee.github.io/Test-Addon/Glow-addon/Glow-addon-core.js?v=${(Date.now()/10000).toFixed(0)}`);
