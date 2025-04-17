257a135c6a4709e88481cc5fabbdee784d8ecd73
ReIgnitee <cortanav93@gmail.com>
2025-04-17 17:47:50 +0530

    Add glow-addon base script
    Created initial structure and user script

---
+++ Glow-addon/Glow-addon.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         BC Glow Addon
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Adds a glow effect around your character
+// @author       You
+// @match        https://www.bondageprojects.elementfx.com/Club/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("Glow addon is active!");
+})();
