63a4e632edc571cfd7da3082f0c2aa5ff5ebf696
ReIgnitee <cortanav93@gmail.com>
2025-04-18 12:26:45 +0530

    Create Glow-addon.user.js

---
+++ Glow-addon.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Bondage Club Chaos Loader
+// @namespace    https://furryzoi.gitlab.io/
+// @version      1.0
+// @description  Loads the Bondage Club Chaos addon script as a module.
+// @author       You
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match https://bondageprojects.com/*
+// @match https://www.bondageprojects.com/*
+// @match        http://localhost:*/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+import('https://reignitee.github.io/Test-Addon/Glow-addon.mjs?v=${(Date.now()/10000).toFixed(0)}');
